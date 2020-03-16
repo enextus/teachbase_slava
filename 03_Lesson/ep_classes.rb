56fee1826d28bd845a98f57bcf702ba0a3c336c2
enextus <enextus@gmail.com>
2020-03-16 14:42:46 +0100

    upd12

--- 03_Lesson/ep_classes.rb
+++ 03_Lesson/ep_classes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable AsciiComments
+
 # Класс Station (Станция):
 #  Имеет название, которое указывается при ее создании.
 #  Может принимать поезда (по одному за раз).
@@ -60,15 +62,17 @@ class Route
 
   # Метод delete_midway может удалять промежуточную станцию из списка.
   def delete_midway(station)
-    if station != @stations.first && station != @stations.last
-      @stations.delete(station)
-    end
+    # Защита (guard clause) от стирания первой и последней станции.
+    return unless (@stations.first || @stations.last) != station
+
+    # удаление промежуточной станции из списка
+    @stations.delete(station)
   end
 
   # Метод list_stations Может выводить список всех станций по-порядку
   # от начальной до конечной.
   def list_stations
-    @stations.each { |item| puts item.name }
+    @stations.each { |item| puts "#{item.name}, #{item}" }
   end
 end
 
@@ -100,60 +104,91 @@ class Train
     @type = type
     @wagons = wagons
     @current_speed = 0
-    @current_station = current_station
   end
 
-  # Метод speed_gain может набирать скорость.
+  # Метод accelerate может набирать скорость.
   def accelerate(value)
     @current_speed += value if value.positive?
   end
 
-  # Метод stop может тормозить (сбрасывать скорость до нуля).
+  # Метод decelerate может тормозить (сбрасывать скорость до нуля).
   def decelerate(value)
     @current_speed -= value if value <= @current_speed && value.positive?
   end
 
-  # Метод uncouple_wagon может отцеплять вагоны (по одному вагону
-  # за операцию, метод просто уменьшает количество вагонов)
-  # отцепка вагонов может осуществляться только если поезд не движется.
-  def detach_wagon
-    @wagons -= 1 if @current_speed.zero? && @wagons.positive?
-  end
-
-  # Метод hitch_wagon может прицеплять вагоны (по одному вагону
+  # Метод attach_wagon может прицеплять вагоны (по одному вагону
   # за операцию, метод просто увеличивает количество вагонов)
   # прицепка вагонов может осуществляться только если поезд не движется.
   def attach_wagon
     @wagons += 1 if @current_speed.zero?
   end
 
-  # Метод route может принимать маршрут следования (объект класса Route).
+  # Метод detach_wagon может отцеплять вагоны (по одному вагону
+  # за операцию, метод просто уменьшает количество вагонов)
+  # отцепка вагонов может осуществляться только если поезд не движется.
+  def detach_wagon
+    @wagons -= 1 if @current_speed.zero? && @wagons.positive?
+  end
+
+  # Метод getting_route может принимать маршрут следования (объект класса Route).
   # При назначении маршрута поезду, поезд автоматически помещается
   # на первую станцию в маршруте.
-  def route(route)
+  def getting_route(route)
     @route = route
     @current_station = @route.stations.first
+    @current_station.arrive(self)
   end
 
-  # Метод moving_forward может перемещаться между станциями, указанными в
+  # Метод move_forwards может перемещаться между станциями, указанными в
   # маршруте. Перемещение возможно вперед, но только на 1 станцию за раз.
   def move_forwards
-    @current_station = @route.stations.rotate!(1).first
+    # Защита (guard clause) от отсутствия route
+    return unless @current_station
+
+    # Получаем значение индекса актуальной станции из массива stations.
+    current_index = @route.stations.find_index(@current_station)
+
+    # Защита (guard clause) от переполнения значения индекса массива stations.
+    return unless current_index + 1 < @route.stations.count
+
+    # Убытие поезда с актуальной станции, где self это текущий экземпляр
+    # класса Train.
+    @current_station.delete(self)
+
+    # Смена актуальной станции на следующую становящуюся актуальной.
+    @current_station = @route.stations[current_index + 1]
+    # Прибытие поезда на следующую станцию, где self это текущий экземпляр
+    # класса Train.
+    @current_station.arrive(self)
   end
 
-  # Метод moving_back может перемещаться между станциями, указанными в
+  # Метод move_backwards может перемещаться между станциями, указанными в
   # маршруте. Перемещение возможно назад, но только на 1 станцию за раз.
-  def move_byckwards
-    @current_station = @route.stations.rotate!(-1).first
+  def move_backwards
+    #
   end
 
   # Метод previous_station может возвращать предыдущую станцию маршрута.
   def previous_station
-    @route.stations.rotate(-1).first if @current_station != @route.stations.first
+    #
   end
 
   # Метод next_station может возвращать предыдущую станцию маршрута.
   def next_station
-    @route.stations.rotate(1).first if @current_station != @route.stations.last
+    #
   end
 end
+
+# seed data
+station_start = Station.new("Arhangelsk")
+station_stop = Station.new("Kiev")
+station_middle_01 = Station.new("Petersburg")
+station_middle_02 = Station.new("Vitebsk")
+station_middle_03 = Station.new("Minsk")
+route_nord_south = Route.new(station_start, station_stop)
+route_nord_south.midway(station_middle_01)
+route_nord_south.midway(station_middle_02)
+route_nord_south.midway(station_middle_03)
+route_nord_south.stations
+train_01_a_k_cargo = Train.new("01.Arch_Kiev", "cargo", 12)
+# rubocop:enable AsciiComments
